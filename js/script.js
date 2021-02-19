3788191f37ac599a0622f3b188d465f2cf2616cd
dakota <kotalilyy@gmail.com>
2021-02-19 06:17:34 -0600

    use setInterval to call a function

---
+++ js/script.js
@@ -0,0 +1,9 @@
+setInterval(setClock, 1000);
+
+function setCLock() {
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 
+    60
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+}
